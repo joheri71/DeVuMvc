d2b7193daaaa93628be85e635d9bc88621fcaf21
Johan Eriksson <joheri71@hotmail.com>
2016-12-23 00:20:12 +0100

    Lektion 2 färdig

---
+++ HelloMvc/HelloMvc/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloMvc.Models;
+
+namespace HelloMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.BobCreatedThisValue = "Hello Mvc";
+            ViewBag.BobCreatedThisValue += "Hello Mvc";
+            ViewBag.BobCreatedThisValue += "Johan är bäst";
+            var cars = HelloModel.GetCars();
+
+            return View(cars);
+        }
+    }
+}
